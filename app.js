f7b610edddc922f8e0691c488e3dcb3d4f4633c4
Guilherme Soares <guilherme.simoes@hotmart.com>
2021-02-17 16:49:38 -0300

    Coleta top 98 MFK

--- app.js
+++ app.js
@@ -1,36 +1,55 @@
+var os = require("os");
+var fs = require("fs");
 const { google } = require("googleapis");
 const youtube = google.youtube("v3"); // initialize the Youtube API library
-const { save } = require("./services/fs");
+const { save, readFile } = require("./services/fs");
+
+const key = "AIzaSyCnvsB6AXf0_C6IWB8HSZOkTd0XJ7u8HVw";
 
 async function main() {
-  for (let index = 0; index < 10; index++) {
-    const file = require(`./data/tratamento-precoce-top-500/content${index}.json`)
-    file.items.forEach((video) => {
-      console.log(video.id.videoId)
-    });
-  }
+  readFile("./data/mkf-100-sb.txt", (err, words) => {
+    words
+      .toString()
+      .split(os.EOL)
+      .forEach(async (channel) => {
+        const param =
+          channel.split("/")[0] === "channel" ? "id" : "forUsername";
+        const value = channel.split("/")[1];
+        const result = await getChannelData({ [param]: value });
+        console.log(`Saving channel data: ${param}/${value}`);
+        save(result.data, value);
+      });
+  });
 }
 
-async function getChannelData() {
+async function getChannelData(query) {
   return await youtube.channels.list({
     part:
       "brandingSettings,contentDetails,contentOwnerDetails,id,localizations,snippet,statistics,status,topicDetails",
-    // id: ["UCURffR0e_FPKnDuzQ0XyWhw", "UCJplp5SjeGSdVdwsfb9Q7lQ"],
-    forUsername: "toysandcolors",
-    key: "AIzaSyCnvsB6AXf0_C6IWB8HSZOkTd0XJ7u8HVw",
+    ...query,
+    key,
   });
 }
 
-async function searchByKeyword(pageToken) {
-  console.log(`Next page token: ${pageToken}`);
-  return youtube.search.list({
-    part: "id,snippet",
-    q: "tratamento precoce",
-    key: "AIzaSyCnvsB6AXf0_C6IWB8HSZOkTd0XJ7u8HVw",
-    maxResults: 50,
-    pageToken,
-  });
-}
+// async function main() {
+//   for (let index = 0; index < 10; index++) {
+//     const file = require(`./data/tratamento-precoce-top-500/content${index}.json`)
+//     file.items.forEach((video) => {
+//       console.log(video.id.videoId)
+//     });
+//   }
+// }
+
+// async function searchByKeyword(pageToken) {
+//   console.log(`Next page token: ${pageToken}`);
+//   return youtube.search.list({
+//     part: "id,snippet",
+//     q: "tratamento precoce",
+//     key,
+//     maxResults: 50,
+//     pageToken,
+//   });
+// }
 
 if (module === require.main) {
   main().catch(console.error);
